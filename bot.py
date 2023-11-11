c99d1980b180a83256bb099d238579dca9b15f29
yungjqve <maxnmck@proton.me>
2023-11-11 14:19:37 +0100

    Refactor scraping logic into TipsScraper class with improved async handling

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ import asyncio
 from datetime import datetime, time, timedelta
 from pytz import timezone
 from dotenv import load_dotenv
-import requests
-from lxml import html
+
+from tips_scraper import TipsScraper
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -21,43 +21,16 @@ bot = commands.Bot(command_prefix="$", intents=intents)
 
 last_tip_message = None  # Variable to store the last sent tip message
 
-async def scrape_for_tips():
-    url = 'https://wettbasis.com'
-    response = requests.get(url)
-    tree = html.fromstring(response.content)
-
-    xpath_match = '/html/body/div[1]/div[4]/div/div/main/article/div/div[1]/div[2]/div[3]/div[2]'
-    xpath_bet = '/html/body/div[1]/div[4]/div/div/main/article/div/div[1]/div[2]/div[3]/div[3]'
-
-    element_match = tree.xpath(xpath_match)
-    element_bet = tree.xpath(xpath_bet)
-
-    match = element_match[0].text_content().strip() if element_match else 'Element not found'
-
-    if element_bet:
-        spans = element_bet[0].findall('.//span')
-        if len(spans) > 2:
-            # Concatenate all but the last span for 'bet', removing new lines
-            bet = ' '.join(span.text_content().strip().replace('\n', ' ') for span in spans[:-2])
-            quote = spans[-1].text_content().strip().replace('\n', ' ')  # Use the last span for 'quote'
-        elif len(spans) == 2:
-            bet = spans[0].text_content().strip().replace('\n', ' ')
-            quote = spans[1].text_content().strip().replace('\n', ' ')
-        else:
-            bet = quote = 'Span elements not found'
-    else:
-        bet = quote = 'Parent element not found'
-
-    return match, bet, quote
+scraper = TipsScraper()
+match, bet, quote, date, units = asyncio.run(scraper.scrape_for_tips())
 
 async def create_tip_message():
-    match, bet, quote = await scrape_for_tips()
     tip_message = (
-        "🚨 **TIP OF THE DAY** 🚨\n"
+        f"🚨 **TIP OF THE DAY {date}** 🚨\n"
         f"**Match:** {match}\n"
         f"**Bet:** {bet}\n"
         f"**Quote:** {quote}\n"
-        "**Betting Units:** TBD"
+        f"**Betting Units:** {units}"
     )
     return tip_message
 
